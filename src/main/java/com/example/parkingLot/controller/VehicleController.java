f6f410b4b5ec0b1c5f8be0eeab78ab4ecc45c0ee
Felipe Alves <felipealexandre1408@gmail.com>
2024-03-19 20:29:06 -0300

    Feat: Get on /vehicles/{id} is done. It returns only one vehicle based on the path variable.

--- src/main/java/com/example/parkingLot/controller/VehicleController.java
+++ src/main/java/com/example/parkingLot/controller/VehicleController.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/vehicles")
@@ -22,5 +24,14 @@ public class VehicleController {
     @GetMapping()
     public ResponseEntity<List<Vehicle>> getVehicles() {
         return ResponseEntity.status(HttpStatus.OK).body(vehicleRepository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getVehicleById(@PathVariable (value = "id") Long id) {
+        Optional<Vehicle> vehicle = vehicleRepository.findById(id);
+        if(vehicle.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Vehicle not found.");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(vehicle.get());
     }
 }
