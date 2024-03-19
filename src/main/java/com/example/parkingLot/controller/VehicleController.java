890690f7528947b8896c6cadafd2e1c12837f15b
Felipe Alves <felipealexandre1408@gmail.com>
2024-03-19 20:44:02 -0300

    Feat: Post method on /vehicles is done. It creates a new vehicle. But it won't create a new owner.

--- src/main/java/com/example/parkingLot/controller/VehicleController.java
+++ src/main/java/com/example/parkingLot/controller/VehicleController.java
@@ -1,14 +1,13 @@
 package com.example.parkingLot.controller;
 
 import com.example.parkingLot.vehicle.Vehicle;
+import com.example.parkingLot.vehicle.VehicleDTO;
 import com.example.parkingLot.vehicle.VehicleRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -34,4 +33,12 @@ public class VehicleController {
         }
         return ResponseEntity.status(HttpStatus.OK).body(vehicle.get());
     }
+
+    @PostMapping()
+    public ResponseEntity<Vehicle> createVehicle(@RequestBody VehicleDTO vehicleDTO) {
+        var vehicleModel = new Vehicle();
+        BeanUtils.copyProperties(vehicleDTO, vehicleModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(vehicleRepository.save(vehicleModel));
+    }
+
 }
