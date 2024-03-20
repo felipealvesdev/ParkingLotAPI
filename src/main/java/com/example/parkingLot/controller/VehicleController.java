bd0335f590455cc405978748cf1448a05383442f
Felipe Alves <felipealexandre1408@gmail.com>
2024-03-20 14:09:08 -0300

    Feat: Put method on /vehicles/{id} is ready. It will update the information of the vehicle.

--- src/main/java/com/example/parkingLot/controller/VehicleController.java
+++ src/main/java/com/example/parkingLot/controller/VehicleController.java
@@ -3,6 +3,7 @@ package com.example.parkingLot.controller;
 import com.example.parkingLot.vehicle.Vehicle;
 import com.example.parkingLot.vehicle.VehicleDTO;
 import com.example.parkingLot.vehicle.VehicleRepository;
+import org.apache.coyote.Response;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -51,5 +52,17 @@ public class VehicleController {
         vehicle.get().getOwner().getVehicleList().remove(vehicle.get());
         vehicleRepository.deleteById(id);
         return ResponseEntity.status(HttpStatus.OK).body("Vehicle deleted successfully.");
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateVehicleById(@PathVariable(value = "id") Long id,
+                                                    @RequestBody VehicleDTO vehicleDTO) {
+        Optional<Vehicle> vehicleModel = vehicleRepository.findById(id);
+        if(vehicleModel.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Vehicle not found.");
+        }
+        var vehicle = vehicleModel.get();
+        BeanUtils.copyProperties(vehicleDTO, vehicle);
+        return ResponseEntity.status(HttpStatus.OK).body(vehicleRepository.save(vehicle));
     }
 }
